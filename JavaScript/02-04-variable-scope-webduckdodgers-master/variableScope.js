b598fa66b60084df82ae4203ea2f64c74211cd54
Duffy <web.duckdodgers@gmail.com>
2024-02-21 15:21:40 +0100

    Finish Ex 02-04

--- JavaScript/02-04-variable-scope-webduckdodgers-master/variableScope.js
+++ JavaScript/02-04-variable-scope-webduckdodgers-master/variableScope.js
@@ -1,23 +1,31 @@
 // variableScope.js
+let globalVar;
 
 // Function 1
 function localScopeExample() {
   // Your code here
+  let localVar;
+  return localVar;
 }
 
 // Function 2
 function globalScopeExample() {
   // Your code here
+  return globalVar;
 }
 
 // Function 3
 function blockScopeExample() {
   // Your code here
+  {
+    let blockVar = 20;
+    return blockVar;
+  }
 }
 
 module.exports = {
   localScopeExample,
   globalScopeExample,
   blockScopeExample,
-  globalVar
+  globalVar,
 };
