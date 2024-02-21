ee9addc150de9036fa76794df5d951c67738ae95
Duffy <web.duckdodgers@gmail.com>
2024-02-21 14:33:17 +0100

    Finish Ex 02-03 + Move Node_Modules

--- JavaScript/02-03-variables-naming-rules-webduckdodgers-master/namingRules.js
+++ JavaScript/02-03-variables-naming-rules-webduckdodgers-master/namingRules.js
@@ -3,26 +3,36 @@
 // Function 1
 function validVariableName() {
   // Your code here
+  let name = "Marc";
+  return name;
 }
 
 // Function 2
 function invalidVariableName() {
   // Your code here
+  let n4me = "Marc";
+  return n4me;
 }
 
 // Function 3
 function camelCaseVariableName() {
   // Your code here
+  let firstName = "Marc";
+  return firstName;
 }
 
 // Function 4
 function snake_caseVariableName() {
   // Your code here
+  let snake_case_variable_name = "Marc";
+  return snake_case_variable_name;
 }
 
 // Function 5
 function PascalCaseVariableName() {
   // Your code here
+  let FisrtName = "Marc";
+  return FisrtName;
 }
 
 module.exports = {
